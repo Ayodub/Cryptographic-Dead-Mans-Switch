d377879e167f1403ee7daa423f7b6a977fb24ee3
Ayodub <66164546+Ayodub@users.noreply.github.com>
2020-07-16 18:49:54 +1000

    Add files via upload
    integrated decryption into script

--- main.py
+++ main.py
@@ -13,108 +13,145 @@ import math
 #at a long period such as 1 month, and immediately hide the key. Next the DMS runs, and finally the actual encryption.
 #All of the definitions for encryption are placed at the top, and only the recursive selection and encryption itself are
 #placed last.
-
-
-#define variables early so that I set up an error handling loop
-self_destruct=0
-clock= -1   #this needs to be set at <0 for the error handling loop lower down to work
-
-currentdirectory = os.getcwd()   # Get current directory (don't want to encrypt system files)
-
-def write_key():  #generate encryption key
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-
-def load_key():   #loads the encryption key
-    return open("key.key", "rb").read()
-
-def encrypt(filename, key):  
-    f = Fernet(key)
-
-write_key() # load the previously generated key
-
-key = load_key()  # initialize the Fernet class
-
-f = Fernet(key)   #file_list = os.listdir(currentdirectory)
-
-starttime=time.time() 
-lasttime=starttime 
-
-warning= input("Your  decryption key has been generated. It is recommended that you keep this in a separate location to the \nfiles which you wish to encrypt. Press enter to continue..")
-
-while True:    #infinite loop used to control errors from input
-    try:
-        while self_destruct != "y" and self_destruct != "n":
-            self_destruct= input("Do you want this program to self-delete at end of execution? y/n :")
-        break
-    except ValueError:
-        print("please type 'y' or 'n'")
-
 while True:
-    try:
-        while clock <= 0 and not math.isnan(clock): 
-                    
-            clock = int(input("\n How many seconds until switch is triggered? : "))  #input returns to line 57 before continuing, so the clock=int(input) is important so that the comparison on line 57 works
-        break
-    except ValueError:
-        print("please enter a number greater than 0")
+    decrypt_encrypt=input("Enter 1 for Dead Man Switch Encryption \nEnter 2 for Decryption ")
+
+    if decrypt_encrypt == "2":
+        currentdirectory = os.getcwd()   # Get current directory (don't want to encrypt system files)
+
+        def load_key():   
+            return open("key.key", "rb").read()
+
+        def encrypt(filename, key): 
+            f = Fernet(key)
+
+
+        key = load_key()  
+
+        f = Fernet(key)   
+
+        print('\n Beginning recursive decryption...\n\n')
+
+        for x in glob.glob(currentdirectory +'/**/*/*', recursive=True):    
+
+            filepath = os.path.join(currentdirectory, x)
+            newfile = os.path.join(currentdirectory, os.path.splitext(x)[0])
         
-trigger = int(clock)
+            if os.path.isfile(filepath):   #make sure it is a file, otherwise if it is folder it will give error
+                print('>>> Located: \t' + filepath + '')
+                print('>>> Decrypted: \t' + newfile + '\n')
+                with open(filepath, "rb") as file:            
+                    file_data = file.read()
+            
+                decrypted_data = f.decrypt(file_data)  
 
-def count(): 
-    global s    
-    while s >= 0:
-        print(s)
-        s -= 1
-        time.sleep(1)
+                with open(newfile, "wb") as file:  
+                    file.write(decrypted_data)
 
-def user_reset():  # this is the alert for interaction and the reset trigger when interacted with.
-    global s
-    print(f'Encryption will begin in {trigger} seconds')
-    interact = input()
-    print('input received, reseting.')
-    s = trigger
-    user_reset()
+                os.remove(filepath)  
+    elif decrypt_encrypt=="1":
+        #define variables early so that I set up an error handling loop
+        self_destruct=0
+        clock= -1   #this needs to be set at <0 for the error handling loop lower down to work
+
+        currentdirectory = os.getcwd()   # Get current directory (don't want to encrypt system files)
+
+        def write_key():  #generate encryption key
+            key = Fernet.generate_key()
+            with open("key.key", "wb") as key_file:
+                key_file.write(key)
+
+        def load_key():   #loads the encryption key
+            return open("key.key", "rb").read()
+
+        def encrypt(filename, key):  
+            f = Fernet(key)
+
+        write_key() # load the previously generated key
+
+        key = load_key()  # initialize the Fernet class
+
+        f = Fernet(key)   #file_list = os.listdir(currentdirectory)
+
+        starttime=time.time() 
+        lasttime=starttime 
+
+        warning= input("Your  decryption key has been generated. It is recommended that you keep this in a separate location to the \nfiles which you wish to encrypt. Press enter to continue..")
+
+        while True:    #infinite loop used to control errors from input
+            try:
+                while self_destruct != "y" and self_destruct != "n":
+                    self_destruct= input("Do you want this program to self-delete at end of execution? y/n :")
+                break
+            except ValueError:
+                print("please type 'y' or 'n'")
+
+        while True:
+            try:
+                while clock <= 0 and not math.isnan(clock): 
+                            
+                    clock = int(input("\n How many seconds until switch is triggered? : "))  #input returns to line 57 before continuing, so the clock=int(input) is important so that the comparison on line 57 works
+                break
+            except ValueError:
+                print("please enter a number greater than 0")
+                
+        trigger = int(clock)
+
+        def count(): 
+            global s    
+            while s >= 0:
+                print(s)
+                s -= 1
+                time.sleep(1)
+
+        def user_reset():  # this is the alert for interaction and the reset trigger when interacted with.
+            global s
+            print(f'Encryption will begin in {trigger} seconds')
+            interact = input()
+            print('input received, reseting.')
+            s = trigger
+            user_reset()
 
 
-s = trigger
-x= threading.Thread(target=user_reset)   #for some reason the .start/.join method on the end of the threading didnt work, it needed to be assigned to a variable
-x.start()
-y= threading.Thread(target=count)
-y.start()
+        s = trigger
+        x= threading.Thread(target=user_reset)   #for some reason the .start/.join method on the end of the threading didnt work, it needed to be assigned to a variable
+        x.start()
+        y= threading.Thread(target=count)
+        y.start()
 
-#-------------This is where the encryption script starts------------ Need to fix threading---
+        #-------------This is where the encryption script starts------------ Need to fix threading---
 
-y.join()
+        y.join()
 
-print("Beginning recursive encryption..")
+        print("Beginning recursive encryption..")
 
-for x in glob.glob(currentdirectory +'/**/*/*', recursive=True):    # Main loop to encrypt all files recursively
-# double asterix ** tells program to encrypt all types of files
+        for x in glob.glob(currentdirectory +'/**/*/*', recursive=True):    # Main loop to encrypt all files recursively
+        # double asterix ** tells program to encrypt all types of files
 
-    filepath = os.path.join(currentdirectory, x)
-    newfile = os.path.join(currentdirectory, x + '.aes')
-    # Encrypt
-    if os.path.isfile(filepath):   #make sure it is a file, otherwise if it is folder it will give error
-        print('>>> Located: \t' + filepath + '')
-        print('>>> Encrypted: \t' + newfile + '\n')
-        with open(filepath, "rb") as file:      #rb= "read in binary"
-            file_data = file.read()
-       
-        encrypted_data = f.encrypt(file_data)  
+            filepath = os.path.join(currentdirectory, x)
+            newfile = os.path.join(currentdirectory, x + '.aes')
+            # Encrypt
+            if os.path.isfile(filepath):   #make sure it is a file, otherwise if it is folder it will give error
+                print('>>> Located: \t' + filepath + '')
+                print('>>> Encrypted: \t' + newfile + '\n')
+                with open(filepath, "rb") as file:      #rb= "read in binary"
+                    file_data = file.read()
+            
+                encrypted_data = f.encrypt(file_data)  
 
-        with open(newfile, "wb") as file:  # wb = "write in binary"
-            file.write(encrypted_data)
-        
-        os.remove(filepath)  #removes the old file
+                with open(newfile, "wb") as file:  # wb = "write in binary"
+                    file.write(encrypted_data)
+                
+                os.remove(filepath)  #removes the old file
 
 
-print('\n Encryption complete...\n\n')
+        print('\n Encryption complete...\n\n')
 
-if self_destruct == "y":          #-------------------------------------------------
-    remove(argv[0])              #comment these to make self-destruct not possible 
-elif self_destruct == "n":      #-------------------------------------------------
-    quit()
+        if self_destruct == "y":          #-------------------------------------------------
+            remove(argv[0])              #comment these to make self-destruct not possible 
+        elif self_destruct == "n":      #-------------------------------------------------
+            quit()
 
+    else:
+        print("Please enter either '1 or '2'")
 
